91b614df73bb492b06b014076f249ea7e9d9575b
Kevin Lin <minecrafts96628@gmail.com>
2024-08-23 17:32:05 +0800

    replace button component with charka button

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import GlobalStyle from "./GlobalComponents.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
     <GlobalStyle />
