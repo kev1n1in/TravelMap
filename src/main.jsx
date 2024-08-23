ed2f7fc240a94db78a7c1725e004c07088260951
Kevin Lin <minecrafts96628@gmail.com>
2024-08-23 16:35:13 +0800

    create home page without data

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import GlobalStyle from "./GlobalComponents.jsx";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
@@ -11,5 +12,6 @@ createRoot(document.getElementById("root")).render(
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
+    <GlobalStyle />
   </StrictMode>
 );
