251ebc1383ace489c24b29b92df1fa327bd765a2
Kevin Lin <minecrafts96628@gmail.com>
2024-08-28 17:29:26 +0800

    homepage and single journey delete button update , layout update

--- src/App.jsx
+++ src/App.jsx
@@ -3,49 +3,53 @@ import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import ProtectedRoute from "./utils/ProtectedRoute";
 import Home from "./pages/Home";
 import SingleJourney from "./pages/SingleJourney/index";
 import Login from "./pages/Login";
 
 const theme = createTheme();
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route
-              path="/home"
-              element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/journey"
-              element={
-                <ProtectedRoute>
-                  <SingleJourney />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/journey/:id"
-              element={
-                <ProtectedRoute>
-                  <SingleJourney />
-                </ProtectedRoute>
-              }
-            />
-          </Routes>
-        </Router>
-      </LocalizationProvider>
-    </ThemeProvider>
+    <GoogleOAuthProvider clientId={clientId}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route
+                path="/home"
+                element={
+                  <ProtectedRoute>
+                    <Home />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/journey"
+                element={
+                  <ProtectedRoute>
+                    <SingleJourney />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/journey/:id"
+                element={
+                  <ProtectedRoute>
+                    <SingleJourney />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </Router>
+        </LocalizationProvider>
+      </ThemeProvider>
+    </GoogleOAuthProvider>
   );
 }
 
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,40 +1,88 @@
-// Login.js
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin } from "@react-oauth/google";
 import {
   signInWithGoogle,
   updateUserProfile,
 } from "../../firebase/firebaseService";
-
-const clientId =
-  "385608276124-eve8v6q1t1hb04f62up1ptccq3k2htf7.apps.googleusercontent.com";
+import styled from "styled-components";
+import googleIcon from "./google.png";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleCredentialResponse = async (response) => {
-    try {
-      const userCredential = await signInWithGoogle(response.credential);
-      const user = userCredential.user;
-      await updateUserProfile(user);
-      navigate("/home");
-    } catch (error) {
-      console.error("Authentication with Firebase failed:", error);
-    }
-  };
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (response) => {
+      try {
+        const accessToken = response.access_token;
+        const userCredential = await signInWithGoogle(accessToken);
+        const user = userCredential.user;
+        await updateUserProfile(user);
+        localStorage.setItem("isLoggedIn", "true");
+        navigate("/home");
+      } catch (error) {
+        console.error("Authentication with Firebase failed:", error);
+        setErrorMessage("看起來遇到了些問題,請稍後重試.");
+      }
+    },
+    onError: (error) => {
+      console.log("Login Failed", error);
+      setErrorMessage("看起來遇到了些問題,請稍後重試.");
+    },
+    ux_mode: "popup",
+    prompt: "select_account",
+  });
 
   return (
-    <GoogleOAuthProvider clientId={clientId}>
-      <div>
-        <GoogleLogin
-          onSuccess={handleCredentialResponse}
-          onError={() => console.log("Login Failed")}
-          useOneTap
-        />
-        <p>使用 Google 登入快速訪問</p>
-      </div>
-    </GoogleOAuthProvider>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <GoogleButton onClick={googleLogin}>
+        <GoogleIcon src={googleIcon} alt="Google Icon" />
+        Sign in with Google
+      </GoogleButton>
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </div>
   );
 };
+
+const GoogleIcon = styled.img`
+  width: 20px;
+  height: 20px;
+  margin-right: 8px;
+`;
+
+const GoogleButton = styled.button`
+  display: flex;
+  align-items: center;
+  border-radius: 24px;
+  padding: 10px 20px;
+  border: 1px solid #ddd;
+  color: #333;
+  font-weight: bold;
+  text-transform: none;
+  width: auto;
+  max-width: 300px;
+  justify-content: flex-start;
+  background-color: white;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f5f5f5;
+  }
+`;
+
+const ErrorMessage = styled.div`
+  margin-top: 16px;
+  color: red;
+  font-size: 14px;
+`;
 
 export default Login;
