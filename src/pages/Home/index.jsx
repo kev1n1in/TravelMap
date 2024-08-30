1be104096a5f3a4dd00f9565c1c72a0e41ed76fd
wen527555 <wen527555@gmail.com>
2024-08-30 16:10:17 +0800

    feat:develop homepage layout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,6 @@ import {
 } from "@tanstack/react-query";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import { Card, Button, Input } from "@mui/material";
 import { RingLoader } from "react-spinners";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../firebase/firebaseConfig";
@@ -15,10 +14,13 @@ import {
   deleteJourney,
 } from "../../firebase/firebaseService";
 import defaultImg from "./default-img.jpg";
-import trash from "./trash-bin-white.png";
+import trashPng from "./delete.png";
 import { motion } from "framer-motion";
 import ConfirmDialog from "../../components/ConfirmDialog";
 import AlertMessage from "../../components/AlertMessage";
+import Header from "../../components/Header";
+import searchPng from "./search-interface.png";
+import bannerPng from "./banner.jpg";
 
 const Home = () => {
   const [user, loading, authError] = useAuthState(auth);
@@ -153,36 +155,27 @@ const Home = () => {
   if (loading)
     return (
       <LoaderWrapper>
-        <RingLoader color="#123abc" />
+        <RingLoader color="#57c2e9" />
       </LoaderWrapper>
     );
   if (authError || error)
     return <p>獲取用戶文檔時出錯: {authError?.message || error.message}</p>;
 
   return (
-    <Container>
-      <Title>行程總覽</Title>
-      <Form>
-        <Input placeholder="搜尋行程" onChange={handleSearchChange} />
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => navigate("/journey")}
-          style={{ background: "#57c2e9" }}
-        >
-          新增行程
-        </Button>
-      </Form>
-
-      <GridContainer>
-        {filteredSearch.map((doc) => {
-          return (
-            <Card
-              key={doc.id}
-              style={{ marginBottom: "10px", cursor: "pointer" }}
-              onClick={() => handleCardClick(doc.id)}
-            >
-              <CardContent
+    <>
+      <Header />
+      <Container>
+        <BannerContainer />
+        <SearchContainer>
+          <SearchImg src={searchPng} />
+          <SearchInput placeholder="搜尋行程" onChange={handleSearchChange} />
+        </SearchContainer>
+        <GridContainer>
+          {filteredSearch.map((doc) => {
+            return (
+              <CardContainer
+                key={doc.id}
+                onClick={() => handleCardClick(doc.id)}
                 backgroundImage={
                   doc.journey &&
                   doc.journey.length > 0 &&
@@ -192,133 +185,144 @@ const Home = () => {
                     : defaultImg
                 }
               >
-                <Overlay>
-                  <ImageContainer></ImageContainer>
-                  <JourneyDetailContainer>
-                    <div
-                      style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                      }}
-                    >
-                      <JourneyTitle>{doc.title || "無標題"}</JourneyTitle>
-                    </div>
-                    <JourneyTime>
-                      {journeyTimes[doc.id] &&
-                      journeyTimes[doc.id].start &&
-                      journeyTimes[doc.id].end
-                        ? `${journeyTimes[doc.id].start} ~ ${
-                            journeyTimes[doc.id].end
-                          }`
-                        : "尚未新增行程"}
-                    </JourneyTime>
-                    <DescriptionContainer>
-                      <JourneyDescription variant="body2" color="textSecondary">
-                        {doc.description || "無描述"}
-                      </JourneyDescription>
-                    </DescriptionContainer>
-                  </JourneyDetailContainer>
-                  <RemoveButton
-                    onClick={(event) => {
-                      event.stopPropagation();
-                      handleOpenDialog(doc.id, doc.title);
-                    }}
-                    whileHover={{ scale: 1.2 }}
-                    whileTap={{ scale: 0.8 }}
-                  >
-                    <RemoveImg
-                      src={trash}
-                      alt="刪除"
-                      initial={{ rotate: 0 }}
-                      animate={{ rotate: [0, 20, -20, 0] }}
-                      transition={{ duration: 0.5 }}
-                    />
-                  </RemoveButton>
-                </Overlay>
-              </CardContent>
-            </Card>
-          );
-        })}
+                <JourneyDetailContainer>
+                  <JourneyTitle>{doc.title || "無標題"}</JourneyTitle>
+                  <JourneyTime>
+                    {journeyTimes[doc.id] &&
+                    journeyTimes[doc.id].start &&
+                    journeyTimes[doc.id].end
+                      ? `${journeyTimes[doc.id].start} ~ ${
+                          journeyTimes[doc.id].end
+                        }`
+                      : "尚未新增行程"}
+                  </JourneyTime>
+                  <TextOverlay>
+                    <JourneyDescription variant="body2" color="textSecondary">
+                      {doc.description || "無描述"}
+                    </JourneyDescription>
+                  </TextOverlay>
+                </JourneyDetailContainer>
+                <RemoveImg
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    handleOpenDialog(doc.id, doc.title);
+                  }}
+                  src={trashPng}
+                  alt="刪除"
+                  animate={{ rotate: [0, 20, -20, 0] }}
+                  transition={{ duration: 0.5 }}
+                />
+              </CardContainer>
+            );
+          })}
 
-        {isFetchingNextPage && (
-          <LoaderWrapper>
-            <RingLoader color="#57c2e9" />
-          </LoaderWrapper>
+          {isFetchingNextPage && (
+            <LoaderWrapper>
+              <RingLoader color="#57c2e9" />
+            </LoaderWrapper>
+          )}
+        </GridContainer>
+        <ConfirmDialog
+          open={open}
+          onClose={handleCloseDialog}
+          onConfirm={handleConfirmDelete}
+          title="確認刪除"
+          contentText={
+            <span>
+              您確定要刪除{" "}
+              <span style={{ color: "#d02c2c" }}>{selectedDocName}</span>{" "}
+              嗎？此操作無法撤銷。
+            </span>
+          }
+          confirmButtonText="確定刪除"
+          cancelButtonText="取消"
+          confirmButtonColor="secondary"
+        />
+
+        {alertMessage && (
+          <AlertMessage message={alertMessage} keyProp={alertKey} />
         )}
-      </GridContainer>
-
-      <ConfirmDialog
-        open={open}
-        onClose={handleCloseDialog}
-        onConfirm={handleConfirmDelete}
-        title="確認刪除"
-        contentText={
-          <span>
-            您確定要刪除{" "}
-            <span style={{ color: "#d02c2c" }}>{selectedDocName}</span>{" "}
-            嗎？此操作無法撤銷。
-          </span>
-        }
-        confirmButtonText="確定刪除"
-        cancelButtonText="取消"
-        confirmButtonColor="secondary"
-      />
-
-      {alertMessage && (
-        <AlertMessage message={alertMessage} keyProp={alertKey} />
-      )}
-    </Container>
+      </Container>
+    </>
   );
 };
 
 const Container = styled.div`
   height: 120vh;
   overflow-y: auto;
-  padding: 20px;
+  margin-top: 80px;
 `;
 
-const Overlay = styled.div`
+const BannerContainer = styled.div`
+  background-image: url(${bannerPng});
+  width: 100%;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  height: 150px;
+`;
+
+const SearchContainer = styled.div`
   position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.4);
+  top: 230px;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin: 0px 20px;
+  background-color: rgb(255, 255, 255);
+  border-radius: 8px;
+  padding: 10px 20px;
+  width: 80%;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+  height: 80px;
 `;
 
-const Form = styled.div`
-  margin: 20px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
+const SearchInput = styled.input`
+  border: none;
+  outline: none;
+  padding: 20px 0px 20px 10px;
+  margin: 0;
+  box-shadow: none;
+  background: transparent;
+  width: 80%;
+  border-radius: 8px;
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+const SearchImg = styled.img`
+  width: 20px;
+  height: 20px;
+  margin-left: 20px;
 `;
 
 const GridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  gap: 10px;
+  gap: 20px;
+  margin: 80px 40px 20px 40px;
+  padding: 20px;
 `;
 
-const CardContent = styled.div`
-  display: flex;
-  flex-direction: column;
+const CardContainer = styled.div`
+  border-radius: 13px;
+  height: 250px;
+  overflow: hidden;
+  cursor: pointer;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
   position: relative;
   height: 250px;
+  width: 100%;
   background-image: url(${(props) => props.backgroundImage || defaultImg});
   background-size: cover;
   background-position: center;
-  transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
-
+  border-radius: 13px;
   &:hover {
-    transform: translateY(-4px);
-    box-shadow: 0px 15px 30px rgba(0, 0, 0, 0.2);
+    transform: translateY(-10px);
   }
-`;
-
-const Title = styled.h2`
-  margin-bottom: 20px;
-  font-size: 36px;
 `;
 
 const LoaderWrapper = styled.div`
@@ -329,58 +333,68 @@ const LoaderWrapper = styled.div`
 `;
 
 const JourneyDetailContainer = styled.div`
-  margin: 12px 0 0 8px;
+  margin: 20px;
+  position: absolute;
+  left: 0;
+  bottom: 0;
 `;
 
 const JourneyTitle = styled.h2`
   padding-top: 8px;
-  font-size: 20px;
-  font-weight: 700;
+  font-size: 28px;
+  font-weight: 800;
   color: white;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
 `;
 
-const ImageContainer = styled.div`
-  width: 100%;
-  height: 140px;
-  background-image: url(${(props) => props.backgroundImage});
-  background-size: cover;
-  background-position: center;
-  border-radius: 4px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+const TextOverlay = styled.div`
+  padding: 5px;
+  border-radius: 5px;
+  /* background: linear-gradient(
+    to right,
+    rgba(255, 255, 255, 0.852) 0%,
+    rgba(0, 0, 0, 0) 30%
+  ); */
 `;
 
 const JourneyTime = styled.span`
-  margin: 8px 0;
-  color: white;
-`;
-
-const RemoveButton = styled(motion.button)`
-  position: absolute;
-  right: 16px;
-  bottom: 4px;
-  margin-top: 12px;
-  background: none;
-  border: none;
-  padding: 0;
-  cursor: pointer;
-`;
-
-const RemoveImg = styled(motion.img)``;
-
-const DescriptionContainer = styled.div`
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  -webkit-line-clamp: 1;
-  white-space: normal;
-  max-width: 200px;
+  font-weight: 600;
+  color: #f8f8f8;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7);
+  font-size: 16px;
 `;
 
 const JourneyDescription = styled.span`
   color: #fff;
+  font-size: 16px;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
 `;
 
+const RemoveImg = styled(motion.img)`
+  position: absolute;
+  cursor: pointer;
+  margin: 12px;
+  top: 10px;
+  right: 10px;
+  width: 24px;
+  height: 24px;
+  &:hover {
+    animation: rotate-animation 0.5s;
+  }
+
+  @keyframes rotate-animation {
+    0% {
+      transform: rotate(0deg);
+    }
+    25% {
+      transform: rotate(20deg);
+    }
+    50% {
+      transform: rotate(-20deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+`;
 export default Home;
