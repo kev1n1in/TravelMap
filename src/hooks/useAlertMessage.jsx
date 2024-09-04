c2fa4e084c8e00256161798ba000071c09821268
wen527555 <wen527555@gmail.com>
2024-09-04 16:56:18 +0800

    fix:add type prop

--- src/hooks/useAlertMessage.jsx
+++ src/hooks/useAlertMessage.jsx
@@ -1,10 +1,14 @@
 import { useState, useCallback, useEffect } from "react";
 import AlertMessage from "../components/AlertMessage";
+import PropTypes from "prop-types";
 
 const useAlert = () => {
   const [alertMessages, setAlertMessages] = useState([]);
   const [visibleMessage, setVisibleMessage] = useState(null);
   const addAlert = useCallback((message) => {
+    if (!message) {
+      return null;
+    }
     setAlertMessages((prevMessages) => [...prevMessages, message]);
   }, []);
 
@@ -26,6 +30,10 @@ const useAlert = () => {
     addAlert,
     AlertMessage: () => <AlertMessage message={visibleMessage} />,
   };
+};
+
+AlertMessage.propTypes = {
+  message: PropTypes.string, // 不再是必需
 };
 
 export default useAlert;
